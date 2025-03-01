d3583a4b6ab356039b1fd847f8cb6b07060d2cbb
Sarath Pillai <sarath@slashroot.in>
2025-03-01 18:24:22 +0530

    Adding flag and basic concurrency

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"surge/fetch"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) < 1 {
+	url := flag.String("u", "", "url")
+	concurrency := flag.Int("c", 100, "concurrency")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of Surge:\n surge -u 'https://www.google.com,https://www.example.com'\n")
+	}
+	flag.Parse()
+	urls := strings.Split(*url, ",")
+	if len(urls) < 1 {
 		fmt.Println("At least provide one Host/URL to fetch")
 		os.Exit(1)
 	}
 	ch := make(chan string)
-	for _, url := range args {
-		go fetch.Fetch(url, ch)
+	for _, u := range urls {
+		for range *concurrency {
+			go fetch.Fetch(u, ch)
+		}
 	}
-	for range args {
-		fmt.Println(<-ch)
+	for range urls {
+		for range *concurrency {
+			fmt.Println(<-ch)
+		}
 	}
 }
