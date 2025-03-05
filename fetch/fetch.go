3b2542b6cfbf8c61f3dcd2b905c04f2f03cf2a2b
Sarath Pillai <sarath@slashroot.in>
2025-03-05 19:08:09 +0530

    Fixing Headers & Auth and reorg functions

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -3,7 +3,6 @@ package fetch
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +14,26 @@ var client = &http.Client{
 	Transport: &http.Transport{},
 }
 
+func headerSet(h []string, req *http.Request) {
+	if len(h) > 0 {
+		for _, v := range h {
+			hs := strings.SplitN(v, ":", 2)
+			if len(hs) == 2 {
+				req.Header.Set(strings.TrimSpace(hs[0]), strings.TrimSpace(hs[1]))
+			}
+		}
+	}
+}
+
+func authSet(a string, req *http.Request) {
+	if len(a) > 0 {
+		as := strings.SplitN(a, ":", 2)
+		if len(as) == 2 {
+			req.SetBasicAuth(strings.TrimSpace(as[0]), strings.TrimSpace(as[1]))
+		}
+	}
+}
+
 func Fetch_post(u string, ch chan<- string, m string, ct string, b string, h []string, a string) {
 	if b == "" {
 		req, err := http.NewRequest(m, u, nil)
@@ -22,17 +41,8 @@ func Fetch_post(u string, ch chan<- string, m string, ct string, b string, h []s
 			ch <- fmt.Sprintf("%v", err)
 		}
 		req.Header.Set("Content-Type", ct)
-		if len(h) > 0 {
-			for _, v := range h {
-				pairs := strings.Split(v, ":")
-				req.Header.Set(pairs[0], pairs[1])
-			}
-		}
-		if len(a) > 0 {
-			cred := strings.Split(a, ":")
-			req.SetBasicAuth(cred[0], cred[1])
-		}
-
+		headerSet(h, req)
+		authSet(a, req)
 		start := time.Now()
 		response, err := client.Do(req)
 		if err != nil {
@@ -40,7 +50,7 @@ func Fetch_post(u string, ch chan<- string, m string, ct string, b string, h []s
 
 		}
 		elapsed := time.Since(start).Seconds()
-		nbytes, err := io.Copy(ioutil.Discard, response.Body)
+		nbytes, err := io.Copy(io.Discard, response.Body)
 		if err != nil {
 			ch <- fmt.Sprintf("Error: %v", err)
 		}
@@ -57,23 +67,15 @@ func Fetch_post(u string, ch chan<- string, m string, ct string, b string, h []s
 			ch <- fmt.Sprintf("Error: %v", err)
 		}
 		req.Header.Set("Content-Type", ct)
-		if len(h) > 0 {
-			for _, v := range h {
-				pairs := strings.Split(v, ":")
-				req.Header.Set(pairs[0], pairs[1])
-			}
-		}
-		if len(a) > 0 {
-			cred := strings.Split(a, ":")
-			req.SetBasicAuth(cred[0], cred[1])
-		}
+		headerSet(h, req)
+		authSet(a, req)
 		start := time.Now()
 		response, err := client.Do(req)
 		if err != nil {
 			ch <- fmt.Sprintf("Error:- %v", err)
 		}
 		elapsed := time.Since(start).Seconds()
-		nbytes, err := io.Copy(ioutil.Discard, response.Body)
+		nbytes, err := io.Copy(io.Discard, response.Body)
 		if err != nil {
 			ch <- fmt.Sprintf("Error: %v", err)
 		}
@@ -83,44 +85,22 @@ func Fetch_post(u string, ch chan<- string, m string, ct string, b string, h []s
 }
 
 func Fetch_get(u string, ch chan<- string, m string, h []string, a string) {
-	if len(a) > 0 {
-		req, err := http.NewRequest(m, u, nil)
-		if err != nil {
-			ch <- fmt.Sprintf("Error: %v", err)
-		}
-		for _, v := range h {
-			pairs := strings.Split(v, ":")
-			req.Header.Set(pairs[0], pairs[1])
-		}
-		cred := strings.Split(a, ":")
-		req.SetBasicAuth(cred[0], cred[1])
-		start := time.Now()
-		response, err := client.Do(req)
-		if err != nil {
-			ch <- fmt.Sprintf("Error: %v", err)
-		}
-		elapsed := time.Since(start).Seconds()
-		nbytes, err := io.Copy(ioutil.Discard, response.Body)
-		if err != nil {
-			ch <- fmt.Sprintf("Error: %v", err)
-		}
-		defer response.Body.Close()
-		ch <- fmt.Sprintf("%.2fs	%7d		%s	%d", elapsed, nbytes, u, response.StatusCode)
-	} else {
-		req, err := http.NewRequest(m, u, nil)
-		if err != nil {
-			ch <- fmt.Sprintf("%v", err)
-			os.Exit(1)
-		}
-		start := time.Now()
-		response, err := client.Do(req)
-		nbytes, err := io.Copy(ioutil.Discard, response.Body)
-		elapsed := time.Since(start).Seconds()
-		defer response.Body.Close()
-		if err != nil {
-			ch <- fmt.Sprintf("%v", err)
-			os.Exit(1)
-		}
-		ch <- fmt.Sprintf("%.2fs	%7d		%s	%d", elapsed, nbytes, u, response.StatusCode)
+	req, err := http.NewRequest(m, u, nil)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %v", err)
 	}
+	headerSet(h, req)
+	authSet(a, req)
+	start := time.Now()
+	response, err := client.Do(req)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %v", err)
+	}
+	elapsed := time.Since(start).Seconds()
+	nbytes, err := io.Copy(io.Discard, response.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("Error: %v", err)
+	}
+	defer response.Body.Close()
+	ch <- fmt.Sprintf("%.2fs	%7d		%s	%d", elapsed, nbytes, u, response.StatusCode)
 }
